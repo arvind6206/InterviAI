e09defd84d200bc1e443801364849b485567a5ef
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-25 08:07:58 +0530

    Resume DB

---
+++ server/models/Resume.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+
+const resumeSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    resumeUrl: {
+        type: String,
+        required: true
+    },
+    parsedText: {
+        type: String,
+
+    },
+    skills: [
+        {
+        type: String,
+    }
+    ],
+    projects: [
+        {
+        type: String,
+    }
+    ],
+    experience: {
+        type: String
+    },
+    education: {
+        type: String
+    },
+    certifications: [
+        {
+        type: String
+    }
+    ]
+}, {timestamps: true})
+
+export const ResumeModel = mongoose.model('Resume', resumeSchema)
