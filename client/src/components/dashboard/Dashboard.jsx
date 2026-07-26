acb38cef127757bd70588a9438236d1549d710e0
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-26 10:33:14 +0530

    Latest

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -11,9 +11,11 @@ import axios from "axios";
 
 function Dashboard() {
     const [resume, setResume] = useState(null)
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
         getResume()
+        getProfile()
     })
 
     async function getResume(){
@@ -32,13 +34,30 @@ function Dashboard() {
             console.log(error)
         }
     }
+
+    async function getProfile(){
+        try {
+            const token = localStorage.getItem("token")
+
+            const response = await axios.get(
+                `{import.meta.env.VITE_BACKEND_URL}/api/v1/user/profile`,{
+                    headers: {
+                        token
+                    }
+                }
+            )
+            setUser(response.data.user)
+        } catch (error) {
+            console.log(error)
+        }
+    }
   return (
     <>
        <div className="flex min-h-screen bg-[#000E24]">
     <Sidebar />
 
     <div className="flex-1 p-8 overflow-y-auto">
-      <Hero />
+      <Hero user={user}/>
 
       <div className="grid grid-cols-2 gap-6 mt-8">
         <ResumeStatus />
--- client/src/components/dashboard/Hero.jsx
+++ client/src/components/dashboard/Hero.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
 const Hero = ({ user }) => {
+    const initials = user?.name
+        ? user.name
+            .split(" ")
+            .map(word => word[0])
+            .join("")
+            .toUpperCase()
+            : "U";
   return (
     <div className="flex items-start justify-between mb-8 m-5">
       <div>
         <h1 className="text-4xl font-bold text-white">
-          Welcome back, {user?.name || "Arvind"} 👋
+          Welcome back, {user?.name || "User"} 👋
         </h1>
 
         <p className="text-slate-400 mt-2 text-lg">
@@ -14,13 +21,7 @@ const Hero = ({ user }) => {
       </div>
 
       <div className="w-12 h-12 rounded-full bg-blue-700 flex items-center justify-center text-white font-semibold text-lg">
-        {user?.name
-          ? user.name
-              .split(" ")
-              .map((n) => n[0])
-              .join("")
-              .toUpperCase()
-          : "AR"}
+        {initials}
       </div>
     </div>
   );
