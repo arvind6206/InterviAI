e9867cb3b6ee25909eb3be1457a1189dec9f0aba
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-27 12:49:27 +0530

    voice Assistant

--- client/src/pages/Interview.jsx
+++ client/src/pages/Interview.jsx
@@ -1,23 +1,15 @@
 import VoiceAssistant from "../components/interview/VoiceAssistant";
-import QuestionCard from "../components/interview/QuestionCard";
-import Transcript from "../components/interview/Transcript";
-import Controls from "../components/interview/Controls";
-import Timer from "../components/interview/Timer";
+// import QuestionCard from "../components/interview/QuestionCard";
+// import Transcript from "../components/interview/Transcript";
+// import Controls from "../components/interview/Controls";
+// import Timer from "../components/interview/Timer";
 
 function Interview() {
   return (
-    <div className="min-h-screen bg-slate-950 text-white p-8">
-
-      <VoiceAssistant />
-
-      <Timer />
-
-      <QuestionCard />
-
-      <Transcript />
-
-      <Controls />
-
+    <div className="min-h-screen bg-[#000E24] flex items-center jutify-center p-10">
+      <div>
+        <VoiceAssistant />
+      </div>
     </div>
   );
 }
